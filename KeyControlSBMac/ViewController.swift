8db64e39642389e7ab6e512311b21b183b304423
bawnorton <bawnorton@me.com>
2021-12-05 02:24:26 -0800

    Initial Commit

---
+++ KeyControlSBMac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  KeyControlSBMac
+//
+//  Created by Benjamin Norton on 2021-12-05.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
